5fc5e6a051b05a6f700afc9f0148ab890feac555
jlutz9 <joao.lutz@aluno.ifsp.edu.br>
2018-09-14 22:33:30 -0300

    Atualização - Aula 14/09

--- app/src/main/java/br/edu/ifsp/scl/sdm/listacontatossdm/view/ContatoActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/listacontatossdm/view/ContatoActivity.java
@@ -1,15 +1,112 @@
 package br.edu.ifsp.scl.sdm.listacontatossdm.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import br.edu.ifsp.scl.sdm.listacontatossdm.R;
+import br.edu.ifsp.scl.sdm.listacontatossdm.model.Contato;
+
+import static br.edu.ifsp.scl.sdm.listacontatossdm.view.ListaContatosActivity.EDITAR_CONTATO;
 
 public class ContatoActivity extends AppCompatActivity {
+
+    private EditText nomeEditText;
+    private EditText enderecoEditText;
+    private EditText telefoneEditText;
+    private EditText emailEditText;
+
+    private Button cancelarButton;
+    private Button salvarButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contato);
+
+        nomeEditText = findViewById(R.id.nomeEditText);
+        enderecoEditText = findViewById(R.id.enderecoEditText);
+        telefoneEditText = findViewById(R.id.telefoneEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+
+        cancelarButton = findViewById(R.id.cancelarButton);
+        salvarButton = findViewById(R.id.salvarButton);
+
+        // setando listeners dos botões
+
+        cancelarButton.setOnClickListener(cancelarClickListener);
+        salvarButton.setOnClickListener(salvarClickListener);
+
+        String subtitulo;
+        Contato contato = (Contato) getIntent().getSerializableExtra(ListaContatosActivity.CONTATO_EXTRA);
+
+        if(contato != null) {
+            boolean edicao = getIntent().getBooleanExtra(ListaContatosActivity.EDITAR_CONTATO, false);
+
+            if(edicao) {
+                //Modo edição
+                subtitulo = "Editar contato";
+                modoDetalheEdicao(contato, true);
+            } else {
+                //Modo detalhes
+                subtitulo = "Detalhes do contato";
+                modoDetalheEdicao(contato, false);
+            }
+        } else {
+            //Modo cadastro
+            subtitulo = "Novo contato";
+        }
+        // Setando subtítulo
+        getSupportActionBar().setSubtitle(subtitulo);
     }
+
+    private void modoDetalheEdicao(Contato contato, boolean edicao) {
+        nomeEditText.setText(contato.getNome());
+        nomeEditText.setEnabled(edicao);
+        enderecoEditText.setText(contato.getEndereco());
+        enderecoEditText.setEnabled(edicao);
+        telefoneEditText.setText(contato.getTelefone());
+        telefoneEditText.setEnabled(edicao);
+        emailEditText.setText(contato.getEmail());
+        emailEditText.setEnabled(edicao);
+        if (edicao) {
+            salvarButton.setVisibility(View.VISIBLE);
+        } else {
+            salvarButton.setVisibility(View.GONE);
+        }
+    }
+
+    private View.OnClickListener cancelarClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            setResult(RESULT_CANCELED);
+            finish();
+        }
+    };
+
+    private View.OnClickListener salvarClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Contato novoContato = getContato();
+            Intent resultadoIntent = new Intent();
+            resultadoIntent.putExtra(ListaContatosActivity.CONTATO_EXTRA, novoContato);
+            int position = getIntent().getIntExtra(ListaContatosActivity.CONTATO_POSITION, -1);
+            resultadoIntent.putExtra(ListaContatosActivity.CONTATO_POSITION, position);
+            setResult(RESULT_OK, resultadoIntent);
+            finish();
+        }
+    };
+
+    private Contato getContato() {
+        Contato novoContato = new Contato();
+        novoContato.setNome(nomeEditText.getText().toString());
+        novoContato.setEndereco(enderecoEditText.getText().toString());
+        novoContato.setTelefone(telefoneEditText.getText().toString());
+        novoContato.setEmail(emailEditText.getText().toString());
+        return novoContato;
+    }
+
 }
--- app/src/main/java/br/edu/ifsp/scl/sdm/listacontatossdm/view/ListaContatosActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/listacontatossdm/view/ListaContatosActivity.java
@@ -1,5 +1,6 @@
 package br.edu.ifsp.scl.sdm.listacontatossdm.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +21,15 @@ import br.edu.ifsp.scl.sdm.listacontatossdm.adapter.ListaContatosAdapter;
 import br.edu.ifsp.scl.sdm.listacontatossdm.model.Contato;
 
 public class ListaContatosActivity extends AppCompatActivity {
+
+    //Request Code para abertura da tela Contato Activity- MODO Novo Contato
+    private final int NOVO_CONTATO_REQUEST_CODE = 0;
+    private final int EDITAR_CONTATO_REQUEST_CODE = 1;
+
+    //CONSTANTE para passar parametros para tela ContatoActivity - MODO Detalhes
+    public static final String CONTATO_EXTRA = "CONTATO_EXTRA";
+    public static final String EDITAR_CONTATO = "EDITAR_CONTATO";
+    public static final String CONTATO_POSITION = "CONTATO_POSITION";
 
     private ListView listaContatosListView;
     private List<Contato> listaContatos;
@@ -32,16 +43,27 @@ public class ListaContatosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_contatos);
 
         listaContatosListView = findViewById(R.id.listaContatosListView);
-
         listaContatos = new ArrayList<>();
-        preencheListaContatos();
 
         //ArrayAdapter<Contato> listaContatosAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaContatos);
         listaContatosAdapter = new ListaContatosAdapter(this, listaContatos);
         listaContatosListView.setAdapter(listaContatosAdapter);
 
+        listaContatosListView.setOnItemClickListener(onContatoClickListener);
+
         registerForContextMenu(listaContatosListView);
     }
+
+    private AdapterView.OnItemClickListener onContatoClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Contato contato = (Contato) parent.getAdapter().getItem(position);
+            Intent detalhesContatoIntent = new Intent(ListaContatosActivity.this, ContatoActivity.class);
+
+            detalhesContatoIntent.putExtra(CONTATO_EXTRA, contato);
+            startActivity(detalhesContatoIntent);
+        }
+    };
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
@@ -56,6 +78,11 @@ public class ListaContatosActivity extends AppCompatActivity {
 
         switch(item.getItemId()) {
             case R.id.editarContatoMenuItem:
+                Intent editarContatoIntent = new Intent(ListaContatosActivity.this, ContatoActivity.class);
+                editarContatoIntent.putExtra(CONTATO_EXTRA, contato);
+                editarContatoIntent.putExtra(EDITAR_CONTATO, true);
+                editarContatoIntent.putExtra(CONTATO_POSITION, infoMenu.position);
+                startActivityForResult(editarContatoIntent, EDITAR_CONTATO_REQUEST_CODE);
                 return true;
             case R.id.ligarParaContatoMenuItem:
                 return true;
@@ -81,6 +108,9 @@ public class ListaContatosActivity extends AppCompatActivity {
             case R.id.configuracaoMenuItem:
                 return true;
             case R.id.novoContatoMenuItem:
+                //abrindo tela de novo contato
+                Intent novoContatoIntent = new Intent("NOVO_CONTATO_ACTION");
+                startActivityForResult(novoContatoIntent, NOVO_CONTATO_REQUEST_CODE);
                 return true;
             case R.id.sairMenuItem:
                 finish();
@@ -89,12 +119,48 @@ public class ListaContatosActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void preencheListaContatos() {
-        for(int i = 0;i < 20;i++) {
-            listaContatos.add(new Contato(
-                    "C" + i,"IFSP","1234","@ifsp"
-            ));
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case NOVO_CONTATO_REQUEST_CODE:
+                if(resultCode == RESULT_OK) {
+                    //recupera o contato do Intent data
+                    Contato novoContato = (Contato) data.getSerializableExtra(CONTATO_EXTRA);
+                    //Atualizo a lista
+                    if(novoContato != null) {
+                        listaContatos.add(novoContato);
+                        //listaContatosAdapter.add(novoContato);
+                        //Notifico adapter
+                        listaContatosAdapter.notifyDataSetChanged();
+                        Toast.makeText(this, "Contato cadastrado", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    if(resultCode == RESULT_CANCELED) {
+                        Toast.makeText(this, "Cadastro cancelado", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+            case EDITAR_CONTATO_REQUEST_CODE:
+                if(resultCode == RESULT_OK) {
+                    //recupera o contato do Intent data
+                    Contato contatoEditado = (Contato) data.getSerializableExtra(CONTATO_EXTRA);
+                    //Atualizo a lista
+                    if(contatoEditado != null) {
+                        //listaContatosAdapter.
+                        //Notifico adapter
+                        int position = data.getIntExtra(CONTATO_POSITION, -1);
+                        listaContatos.set(position, contatoEditado);
+                        listaContatosAdapter.notifyDataSetChanged();
+                        Toast.makeText(this, "Contato editado", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    if(resultCode == RESULT_CANCELED) {
+                        Toast.makeText(this, "Cadastro cancelado", Toast.LENGTH_SHORT).show();
+                    }
+                }
         }
+
+
     }
 
 }
